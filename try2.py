32b09cdda1f678b9494597668d6d57a692470ea4
vipin kumar gautam <vipinkumargautam@vipins.local>
2024-07-16 02:06:24 +0530

    flask added

--- try2.py
+++ try2.py
@@ -11,7 +11,7 @@ from selenium.webdriver.chrome.options import Options
 
 def check_user_existence(phone_number):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
